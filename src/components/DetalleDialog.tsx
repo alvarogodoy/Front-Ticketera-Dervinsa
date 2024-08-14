0a7edb21f47a38eb3eecbc799f136abb4d74408a
BonosoLaraUTN <bonoso.lara@alumnos.frm.utn.edu.ar>
2024-08-14 20:48:13 -0300

    cambio gerentepage

--- src/components/DetalleDialog.tsx
+++ src/components/DetalleDialog.tsx
@@ -7,17 +7,24 @@ import {
   Modal,
   Select,
   SelectChangeEvent,
+  Tooltip,
   Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
 } from "@mui/material";
 import Ticket from "../types/Ticket";
 import CloseIcon from "@mui/icons-material/Close";
 import { useEffect, useState } from "react";
 import Prioridad from "../types/enums/Prioridad";
 import Estado from "../types/enums/Estado";
-import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import { updateTicket } from "../services/TicketService";
 import Rol from "../types/enums/Rol";
 import { useAuth } from "../context/AuthContext";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 interface DetalleDialogProps {
   onClose: () => void;
@@ -31,6 +38,7 @@ const DetalleDialog: React.FC<DetalleDialogProps> = ({
   ticket,
 }) => {
   const [prioridadColor, setPrioridadColor] = useState<string>("");
+  const [confirmDialogOpen, setConfirmDialogOpen] = useState<boolean>(false);
   const { user } = useAuth();
 
   const handleEstadoChange = async (event: SelectChangeEvent) => {
@@ -46,6 +54,27 @@ const DetalleDialog: React.FC<DetalleDialogProps> = ({
     onClose();
   };
 
+  const handleDeleteTicket = async () => {
+    if (ticket) {
+      ticket.eliminado = true; // Cambia el estado a eliminado
+      await updateTicket(ticket); // Actualiza el ticket en el servidor
+      onClose(); // Cierra el diálogo
+    }
+  };
+
+  const handleOpenConfirmDialog = () => {
+    setConfirmDialogOpen(true);
+  };
+
+  const handleCloseConfirmDialog = () => {
+    setConfirmDialogOpen(false);
+  };
+
+  const handleConfirmDelete = async () => {
+    await handleDeleteTicket();
+    setConfirmDialogOpen(false);
+  };
+
   useEffect(() => {
     if (ticket?.prioridad === Prioridad.BAJA) {
       setPrioridadColor("#3aa1da");
@@ -56,187 +85,268 @@ const DetalleDialog: React.FC<DetalleDialogProps> = ({
     }
   }, [ticket]);
 
+  const formatDate = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
   return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      aria-labelledby="modal-title"
-      aria-describedby="modal-description"
-    >
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: "80%",
-          maxWidth: 500,
-          bgcolor: "background.paper",
-          borderRadius: 1,
-          boxShadow: 24,
-          p: 2,
-          display: "flex",
-          flexDirection: "column",
-          maxHeight: "80vh", // Limita la altura del modal para que no ocupe toda la pantalla
-          overflow: "auto", // Permite scroll si el contenido es demasiado largo
-        }}
+    <>
+      <Modal
+        open={open}
+        onClose={onClose}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
       >
         <Box
           sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "80%",
+            maxWidth: 500,
+            bgcolor: "background.paper",
+            borderRadius: 1,
+            boxShadow: 24,
+            p: 2,
             display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
+            flexDirection: "column",
+            maxHeight: "80vh",
+            overflow: "auto",
           }}
         >
-          <Typography
-            id="modal-title"
-            variant="h5"
-            component="h2"
+          <Box
             sx={{
-              fontFamily: "Segoe UI Symbol",
-              overflow: "hidden", // Oculta el desbordamiento
-              textOverflow: "ellipsis", // Muestra elipsis si el texto es demasiado largo
-              //whiteSpace: "nowrap", // Evita que el texto se envuelva
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
           >
-            <b>{ticket?.titulo}</b>
-          </Typography>
-          <IconButton onClick={onClose}>
-            <CloseIcon />
-          </IconButton>
-        </Box>
-        {user?.rol === Rol.GERENTE ? (
-          <FormControl variant="standard" required sx={{ marginTop: 1 }}>
-            <Select
-              value={ticket.estado}
-              onChange={handleEstadoChange}
-              sx={{ width: 160, height: 40 }}
+            <Typography
+              id="modal-title"
+              variant="h5"
+              component="h2"
+              sx={{
+                fontFamily: "Segoe UI Symbol",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+              }}
             >
-              <MenuItem value={Estado.POR_HACER}>
-                <Typography sx={{ fontFamily: "Segoe UI Symbol" }}>
-                  <b>POR HACER</b>
-                </Typography>
-              </MenuItem>
-              <MenuItem value={Estado.EN_PROGRESO}>
-                <Typography sx={{ fontFamily: "Segoe UI Symbol" }}>
-                  <b>EN PROGRESO</b>
-                </Typography>
-              </MenuItem>
-              <MenuItem value={Estado.COMPLETADO}>
-                <Typography sx={{ fontFamily: "Segoe UI Symbol" }}>
-                  <b>COMPLETADO</b>
-                </Typography>
-              </MenuItem>
-              <MenuItem value={Estado.RECHAZADO}>
-                <Typography sx={{ fontFamily: "Segoe UI Symbol" }}>
-                  <b>RECHAZADO</b>
-                </Typography>
-              </MenuItem>
-            </Select>
-          </FormControl>
-        ) : null}
+              <b>{ticket?.titulo}</b>
+            </Typography>
+            <IconButton onClick={onClose}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+          {user?.rol === Rol.GERENTE ? (
+            <FormControl variant="standard" required sx={{ marginTop: 1 }}>
+              <Select
+                value={ticket.estado}
+                onChange={handleEstadoChange}
+                sx={{ width: 160, height: 40 }}
+              >
+                <MenuItem value={Estado.POR_HACER}>
+                  <Typography sx={{ fontFamily: "Segoe UI Symbol" }}>
+                    <b>POR HACER</b>
+                  </Typography>
+                </MenuItem>
+                <MenuItem value={Estado.EN_PROGRESO}>
+                  <Typography sx={{ fontFamily: "Segoe UI Symbol" }}>
+                    <b>EN PROGRESO</b>
+                  </Typography>
+                </MenuItem>
+                <MenuItem value={Estado.COMPLETADO}>
+                  <Typography sx={{ fontFamily: "Segoe UI Symbol" }}>
+                    <b>COMPLETADO</b>
+                  </Typography>
+                </MenuItem>
+                <MenuItem value={Estado.RECHAZADO}>
+                  <Typography sx={{ fontFamily: "Segoe UI Symbol" }}>
+                    <b>RECHAZADO</b>
+                  </Typography>
+                </MenuItem>
+              </Select>
+            </FormControl>
+          ) : null}
 
-        <Typography
-          variant="subtitle1"
-          sx={{
-            marginTop: 3,
-            letterSpacing: ".1rem",
-            fontFamily: "Segoe UI Symbol",
-          }}
-        >
-          <b>Detalles</b>
-        </Typography>
-        <Box p={2}>
           <Typography
             variant="subtitle1"
             sx={{
+              marginTop: 3,
+              letterSpacing: ".1rem",
               fontFamily: "Segoe UI Symbol",
-              display: "flex",
-              alignItems: "center",
-              marginBottom: 1.2,
             }}
           >
-            Usuario:
-            <Avatar
-              sx={{ borderRadius: "4px", ml: 1, width: 24, height: 24, mr: 1 }}
-              src={ticket.usuario.urlPic}
-            />
-            {ticket.usuario.email}
+            <b>Detalles</b>
           </Typography>
-          <Typography
-            variant="subtitle1"
-            sx={{
-              fontFamily: "Segoe UI Symbol",
-              display: "flex",
-              alignItems: "center",
-              marginBottom: 1.2,
-            }}
-          >
-            Prioridad:
-            <FiberManualRecordIcon
+          <Box p={2}>
+            <Typography
+              variant="subtitle1"
               sx={{
-                color: prioridadColor,
-                stroke: "#000", // color del contorno
-                strokeWidth: 1, // grosor del contorno
-                fill: prioridadColor, // color de relleno del ícono
-                filter: "brightness(1.2)", // efecto brillante
-                fontSize: 30, // tamaño del ícono (puedes ajustar el valor según sea necesario)
-                marginLeft: 1,
-                mr: 1,
-              }}
-            />
-            {ticket.prioridad}
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            sx={{
-              fontFamily: "Segoe UI Symbol",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            Requerimiento:
-            <Box
-              sx={{
-                bgcolor: "#d3d3d3",
-                color: "#000",
-                borderRadius: "8px",
-                padding: "4px 8px",
-                ml: 1,
-                overflow: "hidden", // Oculta el desbordamiento
-                textOverflow: "ellipsis", // Muestra elipsis si el texto es demasiado largo
-                whiteSpace: "nowrap", // Evita que el texto se envuelva
+                fontFamily: "Segoe UI Symbol",
+                display: "flex",
+                alignItems: "center",
+                marginBottom: 1.5,
               }}
             >
-              {ticket.requerimiento.descripcion}
-            </Box>
-          </Typography>
-        </Box>
-        <Typography
-          variant="subtitle1"
-          sx={{
-            marginTop: 1,
-            letterSpacing: ".1rem",
-            fontFamily: "Segoe UI Symbol",
-          }}
-        >
-          <b>Descripción</b>
-        </Typography>
-        <Box p={2} sx={{ overflow: "auto" }}>
+              Usuario:
+              <Avatar
+                sx={{
+                  borderRadius: "4px",
+                  ml: 1,
+                  width: 24,
+                  height: 24,
+                  mr: 1,
+                }}
+                src={ticket.usuario.urlPic}
+              />
+              {ticket.usuario.email}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              sx={{
+                fontFamily: "Segoe UI Symbol",
+                display: "flex",
+                alignItems: "center",
+                marginBottom: 1,
+              }}
+            >
+              Prioridad:
+              <FiberManualRecordIcon
+                sx={{
+                  color: prioridadColor,
+                  stroke: "#000",
+                  strokeWidth: 1,
+                  fill: prioridadColor,
+                  filter: "brightness(1.2)",
+                  fontSize: 30,
+                  marginLeft: 1,
+                  mr: 1,
+                }}
+              />
+              {ticket.prioridad}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              sx={{
+                fontFamily: "Segoe UI Symbol",
+                display: "flex",
+                alignItems: "center",
+                marginBottom: 1.2,
+              }}
+            >
+              Requerimiento:
+              <Box
+                sx={{
+                  bgcolor: "#d3d3d3",
+                  color: "#000",
+                  borderRadius: "8px",
+                  padding: "4px 8px",
+                  ml: 1,
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  whiteSpace: "nowrap",
+                }}
+              >
+                {ticket.requerimiento.descripcion}
+              </Box>
+            </Typography>
+            {ticket.estado === Estado.POR_HACER ? (
+              <Typography
+                variant="subtitle1"
+                sx={{
+                  fontFamily: "Segoe UI Symbol",
+                  display: "flex",
+                  alignItems: "center",
+                  marginTop: -1.1,
+                  marginBottom: -1.1,
+                  justifyContent: "space-between", // Añadido para espaciar el contenido
+                }}
+              >
+                Fecha: {formatDate(ticket.fechaCreacion)}
+                <Tooltip title={"Eliminar Ticket"}>
+                  <IconButton onClick={handleOpenConfirmDialog}>
+                    <DeleteIcon
+                      sx={{
+                        strokeWidth: 1,
+                        fontSize: 30,
+                        color: "#f96464",
+                      }}
+                    />
+                  </IconButton>
+                </Tooltip>
+              </Typography>
+            ) : (
+              <Typography
+                variant="subtitle1"
+                sx={{
+                  fontFamily: "Segoe UI Symbol",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between", // Añadido para espaciar el contenido
+                }}
+              >
+                Fecha: {formatDate(ticket.fechaCreacion)}
+              </Typography>
+            )}
+          </Box>
           <Typography
             variant="subtitle1"
             sx={{
+              letterSpacing: ".1rem",
               fontFamily: "Segoe UI Symbol",
-              display: "flex",
-              alignItems: "center",
-              whiteSpace: "pre-wrap", // Permite el ajuste del texto largo
             }}
           >
-            {ticket.descripcion}
+            <b>Descripción</b>
           </Typography>
+          <Box p={2} sx={{ overflow: "auto" }}>
+            <Typography
+              variant="subtitle1"
+              sx={{
+                fontFamily: "Segoe UI Symbol",
+                display: "flex",
+                alignItems: "center",
+                whiteSpace: "pre-wrap",
+              }}
+            >
+              {ticket.descripcion}
+            </Typography>
+          </Box>
         </Box>
-      </Box>
-    </Modal>
+      </Modal>
+
+      <Dialog
+        open={confirmDialogOpen}
+        onClose={handleCloseConfirmDialog}
+        aria-labelledby="confirm-dialog-title"
+        aria-describedby="confirm-dialog-description"
+      >
+        <DialogTitle id="confirm-dialog-title">
+          Confirmar Eliminación
+        </DialogTitle>
+        <DialogContent>
+          <Typography variant="body1">
+            ¿Estás seguro de que deseas eliminar este ticket? Esta acción no se
+            puede deshacer.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseConfirmDialog} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleConfirmDelete} color="secondary">
+            Eliminar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
